bee4328ad4c50f3e9d432d6cd330995443893bc8
Damoon Saghian <damoonsaghian84@gmail.com>
2026-05-01 00:08:58 +0330

    Add files via upload

---
+++ ushell/bash_profile.sh
@@ -0,0 +1,18 @@
+script_dir="$(dirname "$(readlink -f "$0")")"
+
+# ask:
+# , auto repair (if no internet and no LAN, setup network; spm update; also if not on tty1, restart tty1)
+# , backup
+# , copy projects
+
+# ask user for lockscreen password, and if correct continue
+
+TMOUT=600 # this is for timeout when in bash prompt
+_precmd() {
+	# this is for timeout for long running processes
+	[ -n "$START" ] && [ "$((SECONDS-START-TMOUT))" -ge 0 ] && exit
+	START="$SECONDS"
+}
+PROMPT_COMMAND=_precmd
+
+. "$script_dir"/bashrc.sh
---
+++ ushell/ushell.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env sh
+
+[ "$1" = priv ] || {
+	exec sudo setpriv --reuid=nu --regid=nu --groups=input,video,audio /usr/local/bin/codev-shell priv
+}
+
+script_dir="$(dirname "$(readlink -f "$0")")"
+
+[ -f /etc/profile ] && . /etc/profile
+for profile_script in /usr/share/profile/*.sh; do
+	[ -r "$profile_script" ] && . "$profile_script"
+done
+
+export TZ="$HOME/.config/tz"
+export LANG="en_US.UTF-8"
+export MUSL_LOCPATH="/usr/share/i18n/locales/musl"
+export SHELL="sudo -u "$USER" /usr/bin/bash --noprofile --norc -i \"$script_dir\"/bashrc.sh"
+export PATH="/usr/local/bin:/usr/bin:/$HOME/.local/bin"
+export XDG_RUNTIME_DIR="/run/user/$(id -u)"
+export DBUS_SESSION_BUS_ADDRESS="unix:path=$XDG_RUNTIME_DIR/bus"
+rm -rf "$XDG_RUNTIME_DIR"
+mkdir -pm 0700 "$XDG_RUNTIME_DIR"
+
+spm srv
+
+umask 022
+
+if [ "$(tty)" = "/dev/tty1" ] && [ "$(id -u)" != 0 ]; then
+	qml6 "$script_dir"/main.qml || bash --noprofile --norc -i "$script_dir"/bash_profile.sh
+else
+	bash --noprofile --norc -i "$script_dir"/bash_profile.sh
+fi
