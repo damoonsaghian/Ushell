d64dc341076bf6e09438df7d905341fbae53ed1a
Damoon Saghian <damoonsaghian84@gmail.com>
2026-06-25 21:43:49 +0330

    Delete chimera directory

--- chimera/mauikit.py
+++
@@ -1,8 +0,0 @@
-# Cbuild file for mauikit package
-
-# deps: kf
-
-# https://invent.kde.org/maui
-
-# filebrowsing texteditor imagetools terminal
-# documents (pdf viewer using qt-poppler)
--- chimera/quickshell.py
+++
@@ -1,7 +0,0 @@
-# Cbuild file for quickShell
-
-# deps: qt6wayland
-
-# https://git.outfoxxed.me/quickshell/quickshell
-# https://git.outfoxxed.me/quickshell/quickshell/src/branch/master/BUILD.md
-# -DSERVICE_PAM=OFF
